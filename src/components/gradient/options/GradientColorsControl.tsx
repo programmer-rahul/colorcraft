fe0921e00159100cd82326ef9e194795e26e331d
Rahul <7678478061k@gmail.com>
2024-05-25 16:43:25 +0530

    added tooltips using Tippy.js

--- src/components/gradient/options/GradientColorsControl.tsx
+++ src/components/gradient/options/GradientColorsControl.tsx
@@ -1,5 +1,6 @@
 import { useGradient } from "../../../context/GradientContext";
 import { getHexCode } from "../../../utils/helper";
+import Tooltip from "../../tippyjs/Tippy";
 import ColorBox from "../reusable/ColorBox";
 
 const GradientColorsControl = () => {
@@ -17,20 +18,23 @@ const GradientColorsControl = () => {
       <p className="text-center pb-2 xl:text-start xl:font-semibold">Colors</p>
 
       <div className="flex justify-between items-center xl:flex-col xl:items-start">
+
         <ColorBox id={0} />
 
-        <div
-          className="random-colors cursor-pointer xl:self-end xl:px-14"
-          onClick={generateRandomColors}
-        >
-          <svg fill="rgb(226 232 240)" width={20} viewBox="0 0 512 512">
-            <path
-              d="M447.1,86.2C400.3,33.4,332.2,0,256,0C114.6,0,0,114.6,0,256h64c0-106.1,85.9-192,192-192c58.5,0,110.4,26.5,145.5,67.8
+        <Tooltip content="Generate random colors">
+          <div
+            className="random-colors cursor-pointer xl:self-end xl:px-14"
+            onClick={generateRandomColors}
+          >
+            <svg fill="rgb(226 232 240)" width={20} viewBox="0 0 512 512">
+              <path
+                d="M447.1,86.2C400.3,33.4,332.2,0,256,0C114.6,0,0,114.6,0,256h64c0-106.1,85.9-192,192-192c58.5,0,110.4,26.5,145.5,67.8
 	            L341.3,192H512V21.3L447.1,86.2z M256,448c-58.5,0-110.4-26.5-145.5-67.8l60.2-60.2H0v170.7l64.9-64.9
 	            c46.8,52.8,115,86.2,191.1,86.2c141.4,0,256-114.6,256-256h-64C448,362.1,362.1,448,256,448z"
-            />
-          </svg>
-        </div>
+              />
+            </svg>
+          </div>
+        </Tooltip>
 
         <ColorBox id={1} />
       </div>
--- src/components/reusable/ImageBtn.tsx
+++ src/components/reusable/ImageBtn.tsx
@@ -1,3 +1,5 @@
+import Tooltip from "../tippyjs/Tippy";
+
 interface BtnProps {
   type: "primary" | "secondary";
   text: string;
@@ -8,17 +10,18 @@ interface BtnProps {
 
 const ImageBtn = ({ type, imgSrc, clickHandler, text, style }: BtnProps) => {
   return (
-    <div
-      className={`px-2 py-1 border font-semibold flex gap-2 rounded-md cursor-pointer items-center  transition-all capitalize ${style} ${
-        type === "primary"
+    <Tooltip content="Generate random gradient">
+      <div
+        className={`px-2 py-1 border font-semibold flex gap-2 rounded-md cursor-pointer items-center  transition-all capitalize ${style} ${type === "primary"
           ? "border-sky-500 text-sky-500 hover:bg-sky-900"
           : "border-orange-500 text-orange-500 hover:bg-orange-900"
-      }`}
-      onClick={clickHandler}
-    >
-      <img src={imgSrc} alt="random" width={20} />
-      <p>{text}</p>
-    </div>
+          }`}
+        onClick={clickHandler}
+      >
+        <img src={imgSrc} alt="random" width={20} />
+        <p>{text}</p>
+      </div>
+    </Tooltip>
   );
 };
 export default ImageBtn;
